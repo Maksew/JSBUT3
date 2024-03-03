2d0164ffe3208e549baa7c93fb44b51dc1b23b11
Maksew <m1x6me@gmail.com>
2024-03-03 19:00:26 +0100

    some updates/fixes

--- server/index.js
+++ server/index.js
@@ -3,19 +3,19 @@
 const Glue = require('@hapi/glue');
 const Exiting = require('exiting');
 const Manifest = require('./manifest');
+const EmailNotificationService = require('../lib/services/email-notification-service');
 
 exports.deployment = async ({ start } = {}) => {
     const manifest = Manifest.get('/', process.env);
     const server = await Glue.compose(manifest, { relativeTo: __dirname });
 
     console.log('Composition du serveur terminée. Démarrage du serveur...');
+    await server.register(EmailNotificationService);
 
     if (start) {
         await Exiting.createManager(server).start();
         console.log(`Server started at ${server.info.uri}`);
-
-        // Assurez-vous que les services sont bien enregistrés et accessibles
-        console.log('Services disponibles sur le serveur:', server.methods); // server.services() n'est pas standard, server.methods si vous utilisez server.method pour enregistrer des méthodes
+        console.log('Services disponibles sur le serveur:', Object.keys(server.registrations));
 
         return server;
     }
@@ -25,7 +25,7 @@ exports.deployment = async ({ start } = {}) => {
 };
 
 if (require.main === module) {
-    exports.deployment({ start: true }).catch(err => {
+    exports.deployment({ start: true }).catch((err) => {
         console.error(err);
         process.exit(1);
     });
